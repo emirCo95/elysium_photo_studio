a7bd913c840fdc42c33183f38d61f07b50a876ca
Emir Cokrlija <144956911+emirCo95@users.noreply.github.com>
2024-11-15 14:41:50 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,23 @@
+import Image from 'next/image';
+import { raleway } from './ui/fonts';
+import Link from 'next/link';
+
 export default function Home() {
-  return <div className="">Home</div>;
+  return (
+    <div className="w-full h-[90vh] flex bg-black overflow-hidden">
+      <div className="w-[30%] flex justify-center items-center">
+        <div className="">
+          <Link href="/">Home</Link>
+        </div>
+      </div>
+      <div className="w-[70%] flex flex-col justify-center items-center">
+        <h1
+          className={`${raleway.className} antialiased text-white text-5xl text-center`}
+        >
+          See the world through our lens.
+        </h1>
+        <Image src="/hero.jpg" alt="hero" width={500} height={500} />
+      </div>
+    </div>
+  );
 }
