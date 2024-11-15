c40111923a04a3a67bb0b5b769d9ee80a3a7ace3
Emir Cokrlija <144956911+emirCo95@users.noreply.github.com>
2024-11-15 18:57:09 -0500

    update Photos.tsx

--- app/components/Photos.tsx
+++ app/components/Photos.tsx
@@ -1,7 +1,25 @@
+import Image from 'next/image';
 import React from 'react';
 
 const Photos = () => {
-  return <div>Photos</div>;
+  return (
+    <div
+      id="photos"
+      className="w-full h-[100vh] flex gap-5 bg-black overflow-hidden"
+    >
+      <div className="w-[30%] lg:w-[80%] grid grid-cols-1 grid-rows-4 lg:grid-cols-2 lg:grid-rows-2 ml-60 md:ml-80">
+        <div className="w-full h-full">
+          <Image
+            className="object-cover"
+            src="/photo_1.jpg"
+            alt="photo_1"
+            width={1920}
+            height={1280}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Photos;
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import About from './components/About';
 import Hero from './components/Hero';
+import Photos from './components/Photos';
 
 export default function Home() {
   return (
     <>
       <Hero />
       <About />
+      <Photos />
     </>
   );
 }
