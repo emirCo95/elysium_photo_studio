145db6ce11445edfef7442d0ca23abea7a44a21d
Emir Cokrlija <144956911+emirCo95@users.noreply.github.com>
2024-11-15 14:42:50 -0500

    Update fonts.ts

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,3 +1,4 @@
-import { Raleway } from 'next/font/google';
+import { Raleway, Anonymous_Pro } from 'next/font/google';
 
 export const raleway = Raleway({ subsets: ['latin'], weight: ['400'] });
+export const anon_pro = Anonymous_Pro({ subsets: ['latin'], weight: ['400'] });
