e3b46a98f8043c9b35e0c1e17679e60e4574c4cc
Emir Cokrlija <144956911+emirCo95@users.noreply.github.com>
2024-11-15 14:24:15 -0500

    update Navbar.tsx

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,7 +1,17 @@
+import Image from 'next/image';
 import React from 'react';
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="w-full h-[10vh] flex justify-between items-center px-10">
+      <div className="">
+        <Image src="/logo.png" alt="logo" width={100} height={80} />
+      </div>
+      <div className="">
+        <span>Elysium Photo Studio 2024.</span>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
