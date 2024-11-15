3d1473f6b7dbf8de5e6945c362e13929af2cf263
Emir Cokrlija <144956911+emirCo95@users.noreply.github.com>
2024-11-15 14:57:14 -0500

    update main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,51 @@
 import Image from 'next/image';
-import { raleway } from './ui/fonts';
+import { anon_pro, raleway } from './ui/fonts';
 import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div className="w-full h-[90vh] flex bg-black overflow-hidden">
-      <div className="w-[30%] flex justify-center items-center">
-        <div className="">
-          <Link href="/">Home</Link>
+    <div className="w-full h-[90vh] flex gap-5 bg-black overflow-hidden">
+      <div className="w-[30%] flex flex-col justify-center items-start gap-5">
+        <div className="bg-white w-[150px] md:w-[300px] h-[50px] rounded-r-3xl flex justify-center items-center">
+          <Link
+            className={`${anon_pro.className} antialiased text-black text-xl`}
+            href="/"
+          >
+            Home
+          </Link>
+        </div>
+        <div className="bg-white w-[150px] md:w-[300px] h-[50px] rounded-r-3xl flex justify-center items-center">
+          <Link
+            className={`${anon_pro.className} antialiased text-black text-xl`}
+            href="/about"
+          >
+            About
+          </Link>
+        </div>
+        <div className="bg-white w-[150px] md:w-[300px] h-[50px] rounded-r-3xl flex justify-center items-center">
+          <Link
+            className={`${anon_pro.className} antialiased text-black text-xl`}
+            href="/photos"
+          >
+            Photos
+          </Link>
+        </div>
+        <div className="bg-white w-[150px] md:w-[300px] h-[50px] rounded-r-3xl flex justify-center items-center">
+          <Link
+            className={`${anon_pro.className} antialiased text-black text-xl`}
+            href="/contact"
+          >
+            Contact
+          </Link>
         </div>
       </div>
       <div className="w-[70%] flex flex-col justify-center items-center">
         <h1
-          className={`${raleway.className} antialiased text-white text-5xl text-center`}
+          className={`${raleway.className} antialiased text-white text-5xl text-center p-2`}
         >
           See the world through our lens.
         </h1>
-        <Image src="/hero.jpg" alt="hero" width={500} height={500} />
+        <Image src="/hero.png" alt="hero" width={500} height={500} />
       </div>
     </div>
   );
