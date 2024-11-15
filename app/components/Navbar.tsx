af8b75f8e339a88d26c24f8deb90ffb2a60235df
Emir Cokrlija <144956911+emirCo95@users.noreply.github.com>
2024-11-15 14:31:45 -0500

    Update Navbar.tsx

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,14 +1,18 @@
 import Image from 'next/image';
 import React from 'react';
+import { raleway } from '../ui/fonts';
+import Link from 'next/link';
 
 const Navbar = () => {
   return (
     <nav className="w-full h-[10vh] flex justify-between items-center px-10">
-      <div className="">
+      <Link href="/">
         <Image src="/logo.png" alt="logo" width={100} height={80} />
-      </div>
+      </Link>
       <div className="">
-        <span>Elysium Photo Studio 2024.</span>
+        <span className={`${raleway.className} antialiased text-xl`}>
+          Elysium Photo Studio 2024.
+        </span>
       </div>
     </nav>
   );
